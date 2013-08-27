4753556039ae656a75bb3a8536614a9fafbca9b9
gozdy <elcarni@gmail.com>
2013-08-26 22:33:02 -0300

    Testing Lights

--- src/com/gozdy/HookIT/GameScreen.java
+++ src/com/gozdy/HookIT/GameScreen.java
@@ -2,8 +2,13 @@ package com.gozdy.HookIT;
 
 import java.util.Iterator;
 
+import box2dLight.ConeLight;
+import box2dLight.PointLight;
+import box2dLight.RayHandler;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,8 +18,10 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
+
 
 public class GameScreen implements Screen {
 	
@@ -36,17 +43,21 @@ public class GameScreen implements Screen {
 	Texture enemyimage;
 	Texture candy;
 	
+	PointLight hookLight;
+	
 	Sprite ropeSprite;
 	Sprite hookSprite;
 	
 	OrthographicCamera camera;
+	World world;
+	RayHandler rayHandler;
 	
 	Array<Enemy> enemies;
 	Array<TextureRegion> candies;
 	
 	Hero hero;
 	float hookAngle = 0;
-	Vector2 gravity = new Vector2(0,-10);
+	Vector2 gravity = new Vector2(0,-9.8f);
 	long lastEnemyTime;
 	float hookTime = 4f;
 	float distancehero = 0f;
@@ -96,8 +107,18 @@ public class GameScreen implements Screen {
 		
 		ropeSprite.setPosition(hero.hook.position.x+0.15f, hero.hook.position.y);
 		ropeSprite.setOrigin(0, 0);
-	
 		
+		
+		world = new World(gravity, false);
+		
+		
+		rayHandler = new RayHandler(world);
+		rayHandler.setCombinedMatrix(camera.combined);
+		rayHandler.setAmbientLight(1);
+	
+//		new ConeLight(rayHandler, 100, Color.CYAN, 20f, WORLD_WIDTH/2, WORLD_HEIGHT, 270, 35);
+		
+		hookLight = new PointLight(rayHandler, 50, Color.WHITE, 1f, hero.hook.position.x+0.2f, hero.hook.position.y+0.2f);
 		
 	}
 
@@ -213,6 +234,8 @@ public class GameScreen implements Screen {
 		enemy.update(delta);
 		}
 		
+		hookLight.setPosition(hero.hook.position.x+0.2f, hero.hook.position.y+0.2f);
+		
 		//RENDERING 
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
@@ -232,6 +255,8 @@ public class GameScreen implements Screen {
 		hookSprite.draw(hgame.batch);
 		hgame.batch.draw(heroimage, hero.position.x, hero.position.y, hero.bounds.width, hero.bounds.height);
 		hgame.batch.end();
+		
+		rayHandler.updateAndRender();
 		
 		
 	}
@@ -274,6 +299,7 @@ heroimg.dispose();
 background.dispose();
 hookimage.dispose();
 hgame.batch.dispose();
+rayHandler.dispose();
 
 	}
 
